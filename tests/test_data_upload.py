db88f21cbbb0ed108c4c84f804fc062ca97a45ed
Woochan Hwang <31177089+woochan-hwang@users.noreply.github.com>
2022-08-08 00:47:01 +0000

    add __init__.py

--- tests/test_data_upload.py
+++ tests/test_data_upload.py
@@ -1,4 +1,5 @@
 """Tests base_model modele handling of uploaded data"""
 from app.components.models import cox_ph_model
 
-CPH = cox_ph_model.CoxProportionalHazardsRegression()
+def test_load_model():
+    CPH = cox_ph_model.CoxProportionalHazardsRegression()
